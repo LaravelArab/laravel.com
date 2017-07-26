5feb219ec4d0b14e10cbdd3c7a2942ab5860d2b6
Atef Ben Ali <atefBB@users.noreply.github.com>
2017-07-26 20:47:59 +0100

    translate some words

--- resources/views/partials/main-nav.blade.php
+++ resources/views/partials/main-nav.blade.php
@@ -1,10 +1,12 @@
 <li class="nav-docs"><a href="/docs">التوثيق</a></li>
 <li class="nav-laracasts"><a href="https://laracasts.com">Laracasts</a></li>
-<li class="nav-laravel-news"><a href="https://laravel-news.com">News</a></li>
+<li class="nav-laravel-news"><a href="https://laravel-news.com">أخبار</a></li>
 <li class="nav-forge"><a href="https://forge.laravel.com">Forge</a></li>
 
 <li class="dropdown community-dropdown">
-	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Conference <span class="caret"></span></a>
+	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+	مؤتمر
+	<span class="caret"></span></a>
 	<ul class="dropdown-menu" role="menu">
 		<li><a href="https://laracon.eu">Laracon EU</a></li>
 		<li><a href="http://laracon.us">Laracon US</a></li>
@@ -13,7 +15,8 @@
 </li>
 
 <li class="dropdown community-dropdown">
-	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Ecosystem <span class="caret"></span></a>
+	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+	Ecosystem <span class="caret"></span></a>
 	<ul class="dropdown-menu" role="menu">
 		<li><a href="https://github.com/laravel/laravel">GitHub</a></li>
 		<li class="divider"></li>
@@ -21,8 +24,8 @@
 		<li><a href="https://lumen.laravel.com">Lumen</a></li>
 		<li><a href="https://spark.laravel.com">Spark</a></li>
 		<li class="divider"></li>
-		<li><a href="https://laracasts.com/discuss">Forums</a></li>
-		<li><a href="https://larajobs.com/?partner=5#/">Jobs</a></li>
+		<li><a href="https://laracasts.com/discuss">منتديات</a></li>
+		<li><a href="https://larajobs.com/?partner=5#/">فرص عمل</a></li>
 		<li><a href="http://www.laravelpodcast.com/">Podcast</a></li>
 		<li><a href="http://larachat.co">Slack</a></li>
 		<li><a href="https://twitter.com/laravelphp">Twitter</a></li>
