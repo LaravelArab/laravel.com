5f88f96a1141b2f7b56af293371c5bc9b7788770
Taylor Otwell <taylor@laravel.com>
2018-05-23 08:51:03 -0500

    wip

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -24,7 +24,7 @@
 <div class="docs-wrapper container">
 
 	<section class="sidebar">
-		<script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=laravelcom" id="_carbonads_js"></script>
+		<script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?serve=CKYILK3E&placement=laravelcom" id="_carbonads_js"></script>
 		<small><a href="#" id="doc-expand" style="font-size: 11px; color: #B8B8B8;">Expand All</a></small>
 		{!! $index !!}
 	</section>
